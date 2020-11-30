ca9d89958ddf95a76ccfa5b3e54519c2f5b872f3
bloic <loic.benos@gmail.com>
2020-12-01 00:48:50 +0100

    quest 5

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/programs", name="program_")
+ */
+
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(int $id): Response
+    {
+            return $this->render('program/show.html.twig', ['id' => $id]);
+    }
+}
