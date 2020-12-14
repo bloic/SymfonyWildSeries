6ce5aa51c59416e6c9f61831c76619e518174830
bloic <loic.benos@gmail.com>
2020-12-14 21:06:12 +0100

    quest 12

--- readme.md
+++ readme.md
@@ -9,3 +9,7 @@ https://drive.google.com/file/d/18W6DFEnDA53QKgUoZ1C4qFttD-9nuP1q/view?usp=shari
 Quête 11
 
 https://drive.google.com/file/d/1X9IyiAdRfID2ACSQKNlMN104F-8ANRb5/view?usp=sharing
+
+Quête 12
+
+https://drive.google.com/file/d/1ZmuRi4cNX7kSnxQZHeM5cuRd4_W_8SYy/view?usp=sharing
---
+++ src/Form/EpisodeType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Episode;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EpisodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title',TextType::class)
+            ->add('number',IntegerType::class)
+            ->add('synopsis',TextType::class)
+            ->add('season', null, ['choice_label' => 'number'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Episode::class,
+        ]);
+    }
+}
